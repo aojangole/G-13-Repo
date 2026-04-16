1bef488d5495fd0cd4c7b53a97222951982d2253
aojangole <arikolojangole@gmail.com>
2026-04-16 05:28:33 +0300

    added student model

--- ILES/ILES_Backend/models.py
+++ ILES/ILES_Backend/models.py
@@ -14,3 +14,12 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    student_no = models.IntegerField(max_length=15, unique=True)
+    reg_no = models.IntegerField(max_length=15, unique=True)
+    course = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.student_no
